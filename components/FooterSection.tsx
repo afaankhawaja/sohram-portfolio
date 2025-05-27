a1d1f5c52df83c0ed237334d48b25011de9f4518
Afaan Ahmad <117567317+afaankhawaja@users.noreply.github.com>
2025-05-27 09:48:49 -0700

    minor chnage

--- components/FooterSection.tsx
+++ components/FooterSection.tsx
@@ -11,7 +11,7 @@ import { useRouter } from "next/navigation";
 
 export default function FooterSection() {
   const [hover, setHover] = useState(false);  
-  const [hoverlog,setHoverLogo]=useState(false)
+  const [hoverlog,setHoverLogo]=useState(true)
   const router=useRouter()
 
   return (
