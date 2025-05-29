ca8dedc3b15bff05473096ea4bcc5041a4e34e66
Afaan Ahmad <117567317+afaankhawaja@users.noreply.github.com>
2025-05-29 09:38:09 -0700

    updated cards

--- app/web-developement/components/PricingPlans.tsx
+++ app/web-developement/components/PricingPlans.tsx
@@ -10,16 +10,16 @@ const PricingPlans = ({setOpen}:props) => {
   return (
     <div className="w-full max-w-[1246px] mx-auto py-10 flex flex-col xl:flex-row justify-between items-end">
       {/* Left 2 Cards: Intro + Base (merged container) */}
-      <div className="flex flex-col max-xl:mb-10 lg:flex-row bg-white border border-r-0 max-xl:rounded-r-xl rounded-r-none rounded-xl overflow-hidden w-full h-[1000px] lg:h-[560px]  lg:min-w-[610px] shadow-md">
+      <div className="flex flex-col max-xl:mb-10 lg:flex-row bg-white border border-r-0 max-xl:rounded-r-xl rounded-r-none rounded-xl overflow-hidden w-full h-[1100px] lg:h-[560px]  lg:min-w-[610px] shadow-md">
         {/* Intro Card */}
-        <div className="relative flex-1 p-[10px] md:p-[32px] ">
+        <div className="relative flex-1 p-[10px] min-h-fit md:p-[32px] ">
           <h3 className="mt-[17px] text-[28px] text-[#231D4F] leading-[100%]">
             $800 <span className="text-[17px]">/ Project</span>
           </h3>
           <p className="mt-4 text-[28px] text-[#231D4F] leading-[100%] font-normal">
-            Intro <span className="ml-[19px] text-[15px] tracking-[0px] leading-[100%] text-[#AA253D]">5-day delivery</span>
+            Intro <span className="pl-[19px] text-[15px] tracking-[0px] leading-[100%] text-[#AA253D]">5-day delivery</span>
           </p>
-          <p className="text-[15px] text-[#848199] mt-[14px] mb-[24px] font-medium h-[54px]">
+          <p className="text-[15px] text-[#848199] mt-[14px] mb-[54px] md:mb-[24px] font-medium h-[54px]">
           best simple online presence for individuals, startups, or small businesses (info/portfolio).          </p>
           <ul className="space-y-[15px] text-[15px] text-[#848199] leading-[100%] tracking-[0px]">
             <li className="flex items-center space-x-[10.65px] h-[22.5px]">
@@ -43,18 +43,18 @@ const PricingPlans = ({setOpen}:props) => {
               <span>1 rerun on the design.</span>
             </li>
           </ul>
-          <button onClick={()=>setOpen(true)} className="absolute bottom-5 mt-[34px] w-[90%] md:w-[220px] mx-auto py-3 rounded-full bg-[#AA253D] text-white hover:bg-[#94273b] transition-all duration-200">
+          <button onClick={()=>setOpen(true)} className="absolute bottom-8 md:bottom-5 mt-[14px] md:mt-[34px] w-[90%] md:w-[220px] mx-auto py-3 rounded-full bg-[#AA253D] text-white hover:bg-[#94273b] transition-all duration-200">
             Choose plan
           </button>
         </div>
 
         {/* Base Card */}
-        <div className="relative flex-1 p-[10px] md:p-[32px]">
-          <h3 className="mt-[17px] text-[28px] text-[#231D4F] leading-[100%]">
+        <div className="relative flex-1 p-[10px] max-md:-mt-5 md:p-[32px]">
+          <h3 className="md:mt-[17px] mt-6 text-[28px] text-[#231D4F] leading-[100%]">
             $1200 <span className="text-[17px] font-normal">/ Project</span>
           </h3>
           <p className="mt-4 text-[28px] text-[#231D4F] leading-[100%] font-normal">
-            Base <span className="ml-[19px] text-[10px] tracking-[0px] leading-[100%] text-[#AA253D]">15-day delivery</span>
+            Base <span className="ml-[19px] text-[15px] tracking-[0px] leading-[100%] text-[#AA253D]">15-day delivery</span>
           </p>
           <p className="text-[15px] text-[#848199] mt-[14px] mb-[45px] md:mb-[24px] font-medium  md:w-[253px] h-[54px]" >
           best for Growing businesses needing a more robust and interactive website with some dynamic elements.
@@ -96,15 +96,15 @@ const PricingPlans = ({setOpen}:props) => {
       </div>
 
       {/* Pro Card (SEPARATE) */}
-      <div className="relative max-xl:mb-10 bg-gradient-to-b from-[#AA253D] to-[#5D0554] mb-2 text-white p-[10px] md:p-[32px] rounded-xl shadow-xl max-xl:w-[100%] w-[100%] md:min-w-[311px] h-[607px] max-md:pb-20 flex flex-col justify-between scale-[1.03]">
+      <div className="relative max-xl:mb-10 bg-gradient-to-b from-[#AA253D] to-[#5D0554] mb-2 text-white p-[10px] md:p-[32px] rounded-xl shadow-xl max-xl:w-[100%] w-[100%] md:min-w-[311px] md:h-[607px] max-md:pb-20 flex flex-col justify-between scale-[1.03]">
         <div className="absolute top-[18px] right-[18px]  bg-[#75112E] text-white px-3 py-1 rounded-full text-[10px] font-normal">
           MOST POPULAR
         </div>
-        <div className="w-full mt-[25px]">
+        <div className="w-full mt-[15px]">
           <h3 className="text-[36px] leading-[46px] tracking-[0px]">
             $1800 <span className="text-[17px] font-normal">/ Project</span>
           </h3>
-          <p className="mt-4 text-[28px]">Pro <span className="ml-[19px] text-[15px] tracking-[0px] leading-[100%] text-white">25-day delivery</span></p>
+          <p className="mt-2 text-[28px]">Pro <span className="ml-[19px] text-[15px] tracking-[0px] leading-[100%] text-white">25-day delivery</span></p>
           <p className="text-[15px] text-white mt-[14px] mb-[22px] font-medium md:w-[284px]">
           Established organizations needing fully customized websites with advanced features and third-party integrations.
           </p>
@@ -154,7 +154,7 @@ const PricingPlans = ({setOpen}:props) => {
             </li>
           </ul>
         </div>
-        <button onClick={()=>setOpen(true)} className="absolute max-md:-bottom-4 max-md:w-[90%] bottom-5  md:w-[220px] max-xl:w-full mx-auto md:-mb-[10px] max-md:mb-10  py-3 rounded-full text-[15px] font-normal border-white border text-white hover:border-[2px] transition-all duration-100 hover:bg-gradient-to-r from-[#dc2430] to-[#7b4397]">
+        <button onClick={()=>setOpen(true)} className="absolute max-md:-bottom-4 max-md:w-[90%] bottom-8  md:w-[220px] max-xl:w-full mx-auto md:-mb-[10px] max-md:mb-10  py-3 rounded-full text-[15px] font-normal border-white border text-white hover:border-[2px] transition-all duration-100 hover:bg-gradient-to-r from-[#dc2430] to-[#7b4397]">
           Choose plan
         </button>
       </div>
