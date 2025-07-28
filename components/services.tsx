aed94ea6b02265ebe84e0618bbd507bd9c2a572d
Afaan Ahmad <117567317+afaankhawaja@users.noreply.github.com>
2025-07-28 15:54:44 +0500

    centerd services content on large screen

--- components/services.tsx
+++ components/services.tsx
@@ -135,7 +135,7 @@ export default function Services() {
 
   return (
     <div
-      className="relative pb-5 w-full mx-auto max-w-[1440px] min-h-[100vh] overflow-hidden"
+      className="relative pb-5 w-full mx-auto xl:content-center max-w-[1440px] min-h-[100vh] overflow-hidden"
       id="Service"
     >
       {/* Background Image */}
