33100a8717be133d376b44380f18510742fc1fda
afaankhawaja <aafaan02@gmail.com>
2025-07-31 21:45:08 +0500

    fixed issue on moobile

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -198,7 +198,7 @@ const Testimonial: React.FC = () => {
           onMouseLeave={handleMouseLeave}
           onMouseUp={handleMouseUp}
           onMouseMove={handleMouseMove}
-          className="max-md:flex-col gap-y-6 flex space-x-[24px] w-full md:min-w-full md:min-h-[264.84px] md:items-center overflow-x-scroll hidden-scrollbar cursor-grab active:cursor-grabbing select-none"
+          className="max-md:flex-col gap-y-6 flex max-lg:gap-x-6 lg:space-x-[24px] w-full md:min-w-full md:min-h-[264.84px] md:items-center overflow-x-scroll hidden-scrollbar cursor-grab active:cursor-grabbing select-none"
           style={{
             scrollBehavior: isDragging ? "auto" : "smooth",
             userSelect: "none",
