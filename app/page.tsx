e3ff077ca0e3224d3d1eb6d50a9212db5112c3f9
afaankhawaja <aafaan02@gmail.com>
2025-08-01 15:49:41 +0500

    removed unused pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,20 @@
-import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
 import Services from "@/components/services";
-import Experience from "@/components/Experience";
 import PortfolioSection from "@/components/PortfolioSection";
 import Testimonial from "@/components/Testimonial";
 import Contact from "@/components/Contact";
-import FooterSection from "@/components/FooterSection";
-import MyWork from "@/components/MyWork";
 import HireMe from "@/components/HireMe";
-import UIAndUX from "@/components/UIAndUX";
-import Motion from "@/components/Motion";
+
 
 export default function Home() {
   return (
     <main className=" md:w-dvh w-dvh min-h-screen  items-center max-xl:px-4">
-      {/* <Navbar /> */}
-
       <Hero />
       <Services />
       <HireMe />
       <PortfolioSection />
       <Testimonial />
       <Contact />
-      {/* <Motion/> */}
-      {/* <MyWork/> */}
     </main>
   );
 }
--- app/web-developement/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import WebDevelopement from "@/components/WebDevelopement"
-
-const page = () => {
-  return <div>{/* <WebDevelopement/> */}</div>;
-};
-
-export default page;
